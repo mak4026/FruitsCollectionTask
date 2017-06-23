e96121e4d587b0fbe17263e1e53ea73c9f001243
mak4026 <chikuwa2135@gmail.com>
2017-06-24 05:37:54 +0900

    graph

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from environment import Environment
 from qLearning import QLearning
 from field import Field
@@ -14,6 +15,24 @@ def convert_keys(obj, convert=str):
         return obj
     return {convert(k): convert_keys(v, convert) for k, v in obj.items()}
 
+def view_history_graph(histories: List[Tuple[List[int], str]], num: int = 10) -> None:
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import numpy as np
+    for h in histories:
+        # なめらかにする
+        x = np.arange(len(h[0]))
+        b = np.ones(num)/num
+        y = np.convolve(np.array(h[0]), b, mode='same')
+        plt.plot(x, y, label=h[1], antialiased=True)
+    plt.title("FruitsCollectionTask")
+    plt.xlabel("Episodes")
+    plt.ylabel("Steps")
+    plt.legend()
+
+    plt.show()
+
+
 f = Field()
 e = Environment(QLearning, f)
 e.play()
@@ -23,3 +42,5 @@ print(f.storage)
 
 with open('q-qvalue.json','w') as f:
     f.write(json.dumps(convert_keys(e.engine.qvalue), indent=4))
+
+view_history_graph([(e.history, e.engine.__class__.__name__)])
